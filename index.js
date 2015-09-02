0ba6181bdb97c973e0f35a5e6be156616fe86213
tristen <tristen.brown@gmail.com>
2015-09-02 15:16:05 -0400

    Divide that value

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ module.exports = React.createClass({
     var value = e.target.value || '0';
     if (value && typeof value === 'string') {
       var a = Math.floor(parseFloat(value));
-      var color = extend(this.state.color, { a: a });
+      var color = extend(this.state.color, { a: a / 100 });
       this.setState({ color: color });
       this.emitOnChange(color);
     }
