3848a81235383791214b4486e04c07e94df0bbc8
Tom MacWright <tom@macwright.org>
2015-09-15 10:27:44 -0700

    Continue refactoring toward native types

--- test/colorfunc.js
+++ test/colorfunc.js
@@ -13,7 +13,7 @@ test('hsv2hex', function(t) {
 });
 
 test('rgb2hsv', function(t) {
-  t.deepEqual(colorfunc.rgb2hsv(0, 0, 0),   { h: 0, s: 0, v: 0 });
+  t.deepEqual(colorfunc.rgb2hsv(0, 0, 0), { h: 0, s: 0, v: 0 });
   t.deepEqual(colorfunc.rgb2hsv(10, 10, 10), { h: 0, s: 0, v: 4 });
   t.deepEqual(colorfunc.rgb2hsv(20, 10, 10), { h: 0, s: 50, v: 8 });
   t.end();
@@ -23,11 +23,5 @@ test('hsv2rgb', function(t) {
   t.deepEqual(colorfunc.hsv2rgb(0, 0, 0), { r: 0, g: 0, b: 0 });
   t.deepEqual(colorfunc.hsv2rgb(10, 10, 10), { b: 23, g: 23, r: 26 });
   t.deepEqual(colorfunc.hsv2rgb(20, 10, 10), { b: 23, g: 24, r: 26 });
-  t.end();
-});
-
-test('zip', function(t) {
-  t.deepEqual(colorfunc.zip(['a'], [1]), { a: 1 });
-  t.deepEqual(colorfunc.zip(['a', 'b'], [1, 2]), { a: 1, b: 2 });
   t.end();
 });
